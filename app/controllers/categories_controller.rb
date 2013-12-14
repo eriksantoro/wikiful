90288570355948f8608e8101c9cba225da886195
ewsantoro <erik.santoro@gmail.com>
2013-12-14 15:45:29 -0500

    Categories - view and controller for index and show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,12 @@
 class CategoriesController <ApplicationController
 
 	def index
+		@categories = Category.order(name: :asc).limit(25)
 	end
 
 	def show
+		@categories = Category.find(params[:id])
+		@articles = Article.find(params[:id])
 	end
 
 end
